d5971ea9b4a3cd60988f0b807952cf15e28a4e22
Eubyt <eubyt+github@proton.me>
2022-09-16 14:58:26 -0300

    Adicionando Google Tag e arrumando ESlint do nextjs

--- workspaces-pages/portfolio/src/pages/_app.tsx
+++ workspaces-pages/portfolio/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeContextProvider } from '../context/theme-color';
 import '../styles/main.css';
