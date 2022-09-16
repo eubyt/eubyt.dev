7caa15d75a3d6516b640766672e56f4578d6258e
Eubyt <eubyt+github@proton.me>
2022-09-16 15:01:47 -0300

    Adicionando Google Tag e arrumando ESlint do nextjs

--- workspaces-pages/other/src/pages/_app.tsx
+++ workspaces-pages/other/src/pages/_app.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-
 import '../styles/main.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+    return (
+        <>
+            {googleAnalyticsId && (
+                <>
+                    <Script
+                        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+                        strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${googleAnalyticsId}');
+                `}
+                    </Script>
+                </>
+            )}
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
