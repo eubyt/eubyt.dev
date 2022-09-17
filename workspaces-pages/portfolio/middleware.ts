41eb0e2e18487870a83eeb8245ddc6dc8279a214
Eubyt <eubyt+github@proton.me>
2022-09-17 11:36:25 -0300

    Update  middleware

--- workspaces-pages/portfolio/middleware.ts
+++ workspaces-pages/portfolio/middleware.ts
@@ -22,13 +22,16 @@ export default async function middleware(req: NextRequest) {
     const currentHost =
         process.env.NODE_ENV === 'production' &&
         hostname?.replace(`.${process.env.ROOT_DOMAIN}`, '');
+
     const hostnameData = getHostnameDataBySubdomain(currentHost);
 
-    if (url.pathname.startsWith(`/_subdomains`) || !hostnameData) {
-        url.pathname = `/404`;
-    } else {
-        console.log('URL 2', req.nextUrl.href);
-        url.pathname = `/_subdomains/${hostnameData.pathName}${url.pathname}`;
+    if (hostnameData) {
+        if (url.pathname.startsWith(`/_subdomains`)) {
+            url.pathname = `/404`;
+        } else {
+            console.log('URL 2', req.nextUrl.href);
+            url.pathname = `/_subdomains/${hostnameData.pathName}${url.pathname}`;
+        }
     }
 
     return NextResponse.rewrite(url);
