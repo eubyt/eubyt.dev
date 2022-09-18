a3a9bba1ca8c4fe0e397ff04bdadb4810af26620
Eubyt <eubyt+github@proton.me>
2022-09-18 19:43:52 -0300

    Adicionando estrutura da rota do URL Shortener

---
+++ api/url_shortener.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"api.eubyt.dev/util"
+)
+
+type UrlShortener struct {
+	Url string
+}
+
+func isUrl(s string) bool {
+	u, err := url.ParseRequestURI(s)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+// Based on https://github.com/eubyt/go.eub.yt
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		decoder := json.NewDecoder(r.Body)
+		var urlShortener UrlShortener
+		err := decoder.Decode(&urlShortener)
+
+		if err != nil || urlShortener.Url == "" {
+			util.JsonHandler(w, r, &util.Response{Message: "Invalid body JSON."}, http.StatusUnprocessableEntity)
+			return
+		}
+
+		if !isUrl(urlShortener.Url) {
+			util.JsonHandler(w, r, &util.Response{Message: "Invalid URL."}, http.StatusUnprocessableEntity)
+			return
+		}
+
+		util.JsonHandler(w, r, urlShortener, http.StatusCreated)
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
---
+++ util/http_response.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func JsonHandler(w http.ResponseWriter, r *http.Request, data interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
