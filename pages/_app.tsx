cbfa8d1c8f5da119e515ef6c3603a8443938cd64
Eubyt <eubyt+github@proton.me>
2022-09-18 13:09:56 -0300

    Google Ads change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/main.css';
 import Script from 'next/script';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
     const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
@@ -9,10 +10,20 @@ const App = ({ Component, pageProps }: AppProps) => {
 
     return (
         <>
+            <Head>
+                <script
+                    async
+                    data-ad-client={googleAdsense}
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                    crossOrigin="anonymous"
+                ></script>
+            </Head>
+
             {googleAnalyticsId && (
                 <>
                     <Script
-                        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+                        data-website-id={googleAnalyticsId}
+                        src={`https://www.googletagmanager.com/gtag/js`}
                         strategy="afterInteractive"
                     />
                     <Script id="google-analytics" strategy="afterInteractive">
@@ -25,16 +36,6 @@ const App = ({ Component, pageProps }: AppProps) => {
                     </Script>
                 </>
             )}
-
-            {googleAdsense && (
-                <Script
-                    async
-                    data-ad-client={googleAdsense}
-                    src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`}
-                    crossOrigin="anonymous"
-                />
-            )}
-
             <Component {...pageProps} />
         </>
     );
